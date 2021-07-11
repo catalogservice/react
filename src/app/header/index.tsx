446b2dd16f6db16ca2ebbcef22b764d1959ff204
Anthony Finix <anthonyfinix@gmail.com>
2021-07-11 17:29:09 +0530

    importertd dependency css

---
+++ src/app/header/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import style from 'header.module.css';
+
+const Header: FC<any> = (props) => {
+    return (
+        <div id="header" className="wrapper">
+
+        </div>
+    )
+}
+
+export default Header
